191c04461877abacd95e2dc6090c26605059638e
sergiomakaruk <sergiomakaruk@gmail.com>
2017-05-01 20:01:33 -0300

    sergio20170501

--- resources/views/front/programs/show.blade.php
+++ resources/views/front/programs/show.blade.php
@@ -4,9 +4,13 @@
 <?php $video=true; ?>
 <?php
 
-$JSON = file_get_contents("https://www.googleapis.com/youtube/v3/videos?part=statistics&id=$content->video_id&key=AIzaSyAHx3IOPRCJnzs4yVHoiE80VypdwPHOGhY");
-$JSON_Data = json_decode($JSON);
-$ytviews = $JSON_Data->items[0]->statistics->viewCount;
+if($content->video_id != 'pendiente'){
+  $JSON = file_get_contents("https://www.googleapis.com/youtube/v3/videos?part=statistics&id=$content->video_id&key=AIzaSyAHx3IOPRCJnzs4yVHoiE80VypdwPHOGhY");
+  $JSON_Data = json_decode($JSON);
+  $ytviews = $JSON_Data->items[0]->statistics->viewCount;
+}
+else $ytviews = 0;
+
 
 ?>
 @include('front.articles.assets.content',$content)
